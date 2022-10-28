9f8600c25472c415ea122bbf8831d470cc02c3c7
Kurt Khris <christiankpegah@gmail.com>
2022-10-28 01:23:47 +0000

    filter

--- script.js
+++ script.js
@@ -9,12 +9,19 @@ fetch('data.json')
     console.log(err);
 });
 
+function titleCase(str) {
+  str = str.toLowerCase().split(' ');
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+  }
+  return str.join(' ');
+}
+
   function appendData(data) {
   var mainContainer = document.getElementById("myData");
   for (var i = 0; i < data.length; i++) {
     var div = document.createElement("div");
-    // div.innerHTML = 'Id: ' + data[i].id + ' ' + data[i].company;
-    div.innerHTML = `<div class="jobs">
+    div.innerHTML = `<div id="ID${data[i].id}" class="jobs">
                 <div class="card">
                     <div class="card-body">
                         <div class="card-header">
@@ -25,7 +32,7 @@ fetch('data.json')
                                 <p><span class="post-time">${data[i].postedAt}</span> <span style="font-size: 40px">.</span> <span class="contract">${data[i].contract}</span></p>
                             </div>
                             <div class="company-role">
-                                <a href="#">${data[i].position}</a>
+                                <a href="${data[i].website}">${data[i].position}</a>
                             </div>
                             <div class="company-name">
                                 <p>${data[i].company}</p>
@@ -40,13 +47,52 @@ fetch('data.json')
                 </div>
             </div>`
     mainContainer.appendChild(div);
+  }
+  document.getElementById("ID13").style.display = "none";
+  document.getElementById("ID14").style.display = "none";
+  document.getElementById("ID15").style.display = "none";
 
-    if (data[i].id === 13 || data[i].id === 14 || data[i].id === 15) {
-        div.style.display = "none";
+  document.getElementById("loadMore").addEventListener("click", function(){
+    document.getElementById("ID13").style.display = "block";
+    document.getElementById("ID14").style.display = "block";
+    document.getElementById("ID15").style.display = "block";
+    document.getElementById("loadMore").style.display = "none";
+  });
+
+  //filter
+    const jobs = document.querySelectorAll('.jobs');
+    
+
+    function filter(){
+    //Get Values
+    const inputTitle = titleCase(document.getElementById("title").value);
+    const inputLocation = titleCase(document.getElementById('location').value);
+    const inputPeriod = titleCase(document.getElementById('period').value);
+
+
+    console.log(inputPeriod);
+
+    for (var i = 0; i < jobs.length; i++) {
+        if(jobs[i].textContent.includes(inputTitle) && jobs[i].textContent.includes(inputLocation) && jobs[i].textContent.includes(inputPeriod)){
+            jobs[i].style.display = "block";
+            console.log(jobs[i].textContent);
+        }else{
+            jobs[i].style.display = "none";
+        }
     }
 
-    document.getElementById("loadMore").addEventListener("click", function () {
-        div.style.display = "inline-block";
-    });
-  }
+    }
+
+document.getElementById("searchFilter").addEventListener("click", function(){
+    filter();
+});
+
+
+
 }
+
+
+
+
+
+
